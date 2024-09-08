1b6d16423a57a43684e8f82a7085454a81363ec9
samins <sarwar347422@gmail.com>
2024-09-08 13:04:11 -0400

    setup cloud credentials and create function to get url to video

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ import os
 from PIL import Image
 import cv2
 import pathlib
-from passwords import *
+
+from dotenv import load_dotenv
+load_dotenv()
 
 import cloudinary
 import cloudinary.uploader
-
-
-cloudinary.config( 
-    cloud_name = CLOUDINARY_CLOUD_NAME, 
-    api_key = CLOUDINARY_API_KEY, 
-    api_secret = CLOUDINARY_API_SECRET, # Click 'View API Keys' above to copy your API secret
-    secure=True
-)
-
-def get_video_url(video):
-    response = cloudinary.uploader.upload(video,resource_type="video")
-    url = response["secure_url"]
-    return url
+import cloudinary.api
 
 current_path = os.getcwd()
 img_dir = "images"
+
+cloudinary.config( 
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"), 
+    api_key = os.getenv("CLOUDINARY_API_KEY"), 
+    api_secret = os.getenv("CLOUDINARY_API_SECRET"), # Click 'View API Keys' above to copy your API secret
+    secure=True
+)
+
+
+def get_video_url(video):
+    response = cloudinary.uploader.upload(video,
+        resource_type = "video", format="mp4")
+    url = response["secure_url"]
+    return url
 
 def get_average_dimension(folder):
     width = 0
@@ -36,8 +40,8 @@ def get_average_dimension(folder):
             height +=h 
             count+=1
 
-    width = width//count
-    height = height//count
+    width = int(width/count)
+    height = int(height/count)
     return width,height
 
 def resize_images(folder, width, height):
@@ -51,6 +55,8 @@ def resize_images(folder, width, height):
             resized_img.save(image_path, 'JPEG', quality=95)
 
 def create_video(folder,name):
+    avg_w, avg_h = get_average_dimension(folder)
+    resize_images(folder,1000,900)
     video_filename = name+".mp4"
     img_arr = [i for i in os.listdir(folder) if i.endswith((".jpg", ".jpeg", ".png"))]
 
